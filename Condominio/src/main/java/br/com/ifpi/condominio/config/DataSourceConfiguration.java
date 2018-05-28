9b0e159c63fd67dc1443fd21075f372537d8b4d9
Icarus09 <i.victorfs@gmail.com>
2018-05-28 17:03:37 -0300

    Configurando a base do projeto

---
+++ Condominio/src/main/java/br/com/ifpi/condominio/config/DataSourceConfiguration.java
@@ -0,0 +1,21 @@
+package br.com.ifpi.condominio.config;
+
+import javax.sql.DataSource;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+@Configuration
+public class DataSourceConfiguration {
+	
+	@Bean
+	public DataSource dataSource() {
+		DriverManagerDataSource dataSource = new DriverManagerDataSource();
+		dataSource.setDriverClassName("org.postgresql.Driver");
+		dataSource.setUrl("jdbc:postgresql://localhost:5432/condominio");
+		dataSource.setUsername("usuario");
+		dataSource.setPassword("usuario");
+		return dataSource;
+	}
+}
---
+++ Condominio/src/test/java/br/com/ifpi/condominio/CondominioApplicationTests.java
@@ -0,0 +1,18 @@
+package br.com.ifpi.condominio;
+
+import org.junit.Test;
+
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CondominioApplicationTests {
+
+	@Test
+	public void contextLoads() {
+		
+	}
+
+}
